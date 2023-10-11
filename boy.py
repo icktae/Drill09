42fdf10181df69d9faba5593006c89276033668a
icktae <taechil77@naver.com>
2023-10-11 21:44:33 +0900

    fin

--- boy.py
+++ boy.py
@@ -152,7 +152,7 @@ class StateMachine:
         self.transitions = {
             Idle: {right_down: Run, left_down: Run, left_up: Run, right_up: Run, time_out: Sleep, a_down : AutoRun},
             Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
-            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle, a_down : AutoRun},
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
             AutoRun: {time_out: Idle}
         }
 
